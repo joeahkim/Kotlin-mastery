b1153e4d34dd3d257ee6cf4762b613c8d8166cd6
joeahkim <joeahkim@gmail.com>
2025-08-05 14:25:27 +0300

    higher order function

--- src/main/kotlin/CollectionsII/higherOrderFunctions/Main.kt
+++ src/main/kotlin/CollectionsII/higherOrderFunctions/Main.kt
@@ -10,10 +10,20 @@ import kotlin.math.PI
 //}
 
 //squaring all even numbers only
+//fun main(){
+//    val numbers = listOf(1, 2, 3, 4, 5, 6)
+//    val evenNumbers = numbers
+//        .filter { it % 2 == 0 }
+//        .map { it * it }
+//    println(evenNumbers)
+//}
+
+//Keep only names starting with the letter "K"
+//Capitalize them all
 fun main(){
-    val numbers = listOf(1, 2, 3, 4, 5, 6)
-    val evenNumbers = numbers
-        .filter { it % 2 == 0 }
-        .map { it * it }
-    println(evenNumbers)
+    val names = listOf("kim", "regina", "kyle", "faith", "derrick")
+    val contained = names
+        .filter { it.startsWith('k') }
+        .map { it.replaceFirstChar { char -> char.uppercaseChar()} }
+    println(contained)
 }
