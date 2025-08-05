aa6ed220a956cbea597a17f1660525b403c0e41e
joeahkim <joeahkim@gmail.com>
2025-08-05 15:17:24 +0300

    All any none

---
+++ src/main/kotlin/CollectionsII/higherOrderFunctions/AnyNoneAll.kt
@@ -0,0 +1,26 @@
+package CollectionsII.higherOrderFunctions
+
+//Print true if any student scored below 50
+//Print true if all students passed (score ≥ 50)
+//Print true if none scored 100
+//fun main(){
+//    val students = listOf(70, 85, 90, 40, 60)
+//    println(students.any { it < 50 })
+//    println(students.all { it > 50 })
+//    println(students.none{it == 100})
+//}
+
+//Check if anyone is under 18
+//Check if all users are above 16
+//Check if no one is older than 60
+fun main(){
+    val users = listOf(
+        "Kim" to 25,
+        "Regina" to 19,
+        "Derrick" to 31,
+        "Faith" to 17
+    )
+    println(users.map { it.second }.any{ it < 18})
+    println(users.map { it.second }.all { it > 18 })
+    println(users.map { it.second }.none { it > 60 })
+}
