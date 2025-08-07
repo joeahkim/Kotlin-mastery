1b2ed9bd6da05da56de7cea76c1b637091362e89
joeahkim <joeahkim@gmail.com>
2025-08-07 11:14:02 +0300

    Flattening Collections

--- src/main/kotlin/CollectionsII/FlatteningNestedCollections/Anagram.kt
+++ src/main/kotlin/CollectionsII/FlatteningNestedCollections/Anagram.kt
@@ -8,10 +8,25 @@ package CollectionsII.FlatteningNestedCollections
 //}
 
 // Group the words by their length, and within each group, sort them alphabetically.
+//fun main(){
+//    val words = listOf("cat", "to", "apple", "dog", "a", "banana", "bat", "an")
+//    val x = words.groupBy { it.length }
+//        .toSortedMap()
+//        .mapValues { it.value.sorted() }
+//    println(x.values.toList())
+//}
+
+//Count how many words start with each letter
+//fun main(){
+//    val words = listOf("apple", "banana", "apricot", "blueberry", "cherry", "avocado", "carrot")
+//    val x = words.groupBy { it.first() }.mapValues { it.value.size }
+//    println(x)
+//}
+
 fun main(){
-    val words = listOf("cat", "to", "apple", "dog", "a", "banana", "bat", "an")
-    val x = words.groupBy { it.length }
-        .toSortedMap()
-        .mapValues { it.value.sorted() }
-    println(x.values.toList())
+    val words = listOf("i", "love", "kotlin", "i", "code", "love", "code", "code")
+    val k = 2
+    val x = words.groupingBy { it }.eachCount().toSortedMap()
+    println(x)
+
 }
