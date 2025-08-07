ee6534ef2f83d74ed4a947ceac9844f8678f8d8d
joeahkim <joeahkim@gmail.com>
2025-08-07 10:54:54 +0300

    Flattening Collections(Anagram)

--- src/main/kotlin/CollectionsII/FlatteningNestedCollections/Anagram.kt
+++ src/main/kotlin/CollectionsII/FlatteningNestedCollections/Anagram.kt
@@ -1,8 +1,17 @@
 package CollectionsII.FlatteningNestedCollections
 
-fun main(){
-    val words = listOf("bat", "tab", "tap", "pat", "cat", "act", "at")
+//fun main(){
+//    val words = listOf("bat", "tab", "tap", "pat", "cat", "act", "at")
+//
+//    val groupedAnagrams = words.groupBy { it.toCharArray().sorted().joinToString("") }
+//    groupedAnagrams.values.forEach { println(it) }
+//}
 
-    val groupedAnagrams = words.groupBy { it.toCharArray().sorted().joinToString("") }
-    groupedAnagrams.values.forEach { println(it) }
+// Group the words by their length, and within each group, sort them alphabetically.
+fun main(){
+    val words = listOf("cat", "to", "apple", "dog", "a", "banana", "bat", "an")
+    val x = words.groupBy { it.length }
+        .toSortedMap()
+        .mapValues { it.value.sorted() }
+    println(x.values.toList())
 }
