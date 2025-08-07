756150fec7d53b9edb6606fe256e68adba069cbe
joeahkim <joeahkim@gmail.com>
2025-08-07 11:39:14 +0300

    Flattening Collections(Zipping and Unzipping)

--- src/main/kotlin/CollectionsII/ZippingAndUnzipping/Students.kt
+++ src/main/kotlin/CollectionsII/ZippingAndUnzipping/Students.kt
@@ -1,10 +1,24 @@
 package CollectionsII.ZippingAndUnzipping
 
+//fun main(){
+//    val names = listOf("Kim", "Regina", "Derrick")
+//    val scores = listOf(90, 85, 95)
+//    val x = names.zip(scores){ names, scores ->
+//        "$names -> $scores"
+//    }.sortedByDescending { it }
+//    println(x)
+//}
+
 fun main(){
-    val names = listOf("Kim", "Regina", "Derrick")
-    val scores = listOf(90, 85, 95)
-    val x = names.zip(scores){ names, scores ->
-        "$names -> $scores"
-    }
-    println(x)
+    val studentData = listOf(
+        "Kim" to 90,
+        "Regina" to 95,
+        "Derrick" to 85,
+        "Faith" to 95
+    )
+    val (name, score) = studentData.unzip()
+    println("name: $name")
+    println("Scores: $score")
+    println("Average Score: ${score.average()}")
+    println("Top Scores: ${studentData.maxByOrNull { it.second }?.first}")
 }
