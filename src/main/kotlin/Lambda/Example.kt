5d4277561ec26b89b3a3775b2101935c11d39dbd
joeahkim <joeahkim@gmail.com>
2025-08-11 15:34:26 +0300

    Lambda Expressions

--- src/main/kotlin/Lambda/Example.kt
+++ src/main/kotlin/Lambda/Example.kt
@@ -1,12 +1,23 @@
 package Lambda
 
 // use a lambda with collection functions to find the maximum product of any two distinct numbers.
-fun main(){
-    val nums = listOf(3, 5, -2, 9, -8)
-    val sortedNums = nums.sorted()
-    val product1 = sortedNums.takeLast(2).reduce { a, b -> a * b }
-    val product2 = sortedNums.take(2).reduce { a, b -> a * b }
+//fun main(){
+//    val nums = listOf(3, 5, -2, 9, -8)
+//    val sortedNums = nums.sorted()
+//    val product1 = sortedNums.takeLast(2).reduce { a, b -> a * b }
+//    val product2 = sortedNums.take(2).reduce { a, b -> a * b }
+//
+//    val maxProduct = maxOf(product1, product2)
+//    println(maxProduct)
+//}
 
-    val maxProduct = maxOf(product1, product2)
-    println(maxProduct)
+fun main(){
+    val words = listOf("Bat", "tab", "tap", "pat", "cat", "Act", "at", "At", "BAT")
+
+    val groupedAnagrams = words.distinct()
+        .groupBy { it.lowercase().toCharArray().sorted().joinToString("") }
+        .mapValues { (_, group) -> group.sorted() }
+        .values
+        .sortedBy { it.first().lowercase() }
+    println(groupedAnagrams)
 }
