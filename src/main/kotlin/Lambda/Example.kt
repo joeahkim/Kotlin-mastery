6eb5ddea911108cf1a3af5adde10a59c345463a6
joeahkim <joeahkim@gmail.com>
2025-08-11 15:59:01 +0300

    Lambda Expressions

--- src/main/kotlin/Lambda/Example.kt
+++ src/main/kotlin/Lambda/Example.kt
@@ -11,13 +11,25 @@ package Lambda
 //    println(maxProduct)
 //}
 
-fun main(){
-    val words = listOf("Bat", "tab", "tap", "pat", "cat", "Act", "at", "At", "BAT")
+//fun main(){
+//    val words = listOf("Bat", "tab", "tap", "pat", "cat", "Act", "at", "At", "BAT")
+//
+//    val groupedAnagrams = words.distinct()
+//        .groupBy { it.lowercase().toCharArray().sorted().joinToString("") }
+//        .mapValues { (_, group) -> group.sorted() }
+//        .values
+//        .sortedBy { it.first().lowercase() }
+//    println(groupedAnagrams)
+//}
 
-    val groupedAnagrams = words.distinct()
-        .groupBy { it.lowercase().toCharArray().sorted().joinToString("") }
-        .mapValues { (_, group) -> group.sorted() }
+fun main() {
+    val nums = listOf(112, 121, 211, 45, 54, 99, 909, -121)
+    val sortedNums = nums
+        .groupBy { Math.abs(it).toString().toCharArray().sorted().joinToString { "" }}
+        .mapValues { (_,group) -> group.sorted() }
         .values
-        .sortedBy { it.first().lowercase() }
-    println(groupedAnagrams)
+        .sortedBy { it.first() }
+    println(sortedNums)
 }
+
+
