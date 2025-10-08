4abfcbde579629e871652f2984dcf6bc11579758
joeahkim <joeahkim@gmail.com>
2025-10-08 11:42:43 +0300

    remembering state

--- app/src/main/java/com/joeahkim/androidbasics/MainActivity.kt
+++ app/src/main/java/com/joeahkim/androidbasics/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -65,7 +66,7 @@ fun Greetings(
 
 @Composable
 fun Greeting(name: String) {
-    val expandable = remember { mutableStateOf(false) }
+    val expandable = rememberSaveable { mutableStateOf(false) }
     val extraPadding = if (!expandable.value) 48.dp else 0.dp
     Surface (
         color = MaterialTheme.colorScheme.primary,
